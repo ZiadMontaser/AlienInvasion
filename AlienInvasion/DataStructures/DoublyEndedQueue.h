e1327bbfecbd4d52bd9f521f9ac335f34e51cffa
mohamed yasser <m.yasser.2004.my@gmail.com>
2024-04-02 18:04:51 +0200

    DS DONE

--- AlienInvasion/DataStructures/DoublyEndedQueue.h
+++ AlienInvasion/DataStructures/DoublyEndedQueue.h
@@ -8,18 +8,22 @@ template <class T>
 class DoublyEndedQueue : public LinkedQueue <T> {
 
 public:
+
+	using LinkedQueue<T>::frontPtr;
+	using LinkedQueue<T>::backPtr;
+
 	template<typename T>
 	inline bool dequeueback(T& backentry)
 	{
-
-		Node <T>* Todelete = LinkedQueue<T>::backPtr;
+		
+		Node <T>* Todelete = backPtr;
 
 		if (Todelete != NULL) {
 			backentry = Todelete->getItem();
-			LinkedQueue<T>::backPtr = Todelete->getPrev();
+			backPtr = Todelete->getPrev();
 
-			if (Todelete == LinkedQueue<T>::frontPtr)	 // Special case: last node in the queue
-				LinkedQueue<T>::frontPtr = nullptr;
+			if (Todelete == frontPtr)	 // Special case: last node in the queue
+				frontPtr = nullptr;
 			delete Todelete;
 			return true;
 		}
@@ -33,17 +37,17 @@ public:
 
 		Node<T>* ToAdd = new Node<T>(toaddfront);
 
-		if (LinkedQueue<T>::backPtr == nullptr) {
-			LinkedQueue<T>::backPtr = ToAdd;
-			LinkedQueue<T>::frontPtr = ToAdd;
+		if (backPtr == nullptr) {
+			backPtr = ToAdd;
+			frontPtr = ToAdd;
 		}
 
 		else {
-			ToAdd->setNext(LinkedQueue<T>::frontPtr);
+			ToAdd->setNext(frontPtr);
 		}
 
-		LinkedQueue<T>::frontPtr->setPrev(ToAdd);
-		LinkedQueue<T>::frontPtr = ToAdd;
+		frontPtr->setPrev(ToAdd);
+		frontPtr = ToAdd;
 
 
 		return true;
